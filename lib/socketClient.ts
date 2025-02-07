433e92b86a56de68ed9e99035d9e8b2777c6327f
mohamedtarek45 <moka76615@gmail.com>
2025-02-07 23:14:19 +0200

    test

--- lib/socketClient.ts
+++ lib/socketClient.ts
@@ -1,4 +1,4 @@
 "use client";
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3000");
